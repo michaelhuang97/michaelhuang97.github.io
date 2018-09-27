01a61fafdfd35c7857a63e7938788025c8e5c9d5
michaelhuang97 <michaelhuang97@gmail.com>
2018-09-27 12:02:50 -0400

    fixed all layout issues

---
+++ Scripts/index.js
@@ -0,0 +1,14 @@
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
